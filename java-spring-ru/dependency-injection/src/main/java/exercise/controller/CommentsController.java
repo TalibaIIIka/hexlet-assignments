b71e1abff6d909ffd9dd29287407447659f00569
@hexlet/cli <support@hexlet.io>
2024-05-07 22:23:29 +0500

    submit java-spring-ru/dependency-injection

---
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -0,0 +1,53 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+
+// BEGIN
+@RestController
+@RequestMapping(path = "/comments")
+public class CommentsController {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping("")
+    public List<Comment> showComments() {
+        return commentRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Comment> showCommentById(@PathVariable long id) {
+        return ResponseEntity.of(commentRepository.findById(id));
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment addComment(@RequestBody Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Comment udpdateComment(@PathVariable long id, @RequestBody Comment updComment) {
+        var searchedComment = commentRepository.findById(id);
+        if (searchedComment.isPresent()) {
+            var comment = searchedComment.get();
+            comment.setBody(updComment.getBody());
+            comment.setPostId(updComment.getPostId());
+            return commentRepository.save(comment);
+        }
+        return updComment;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteComment(@PathVariable long id) {
+        commentRepository.deleteById(id);
+    }
+}
+// END
---
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,73 @@
+package exercise.controller;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+
+// BEGIN
+@RestController
+@RequestMapping(path = "/posts")
+public class PostsController {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping("")
+    public List<Post> showAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Post showPost(@PathVariable long id) {
+        var searchedPost = postRepository.findById(id);
+        if (searchedPost.isEmpty()) {
+            throw new ResourceNotFoundException("Post with id " + id + " not found");
+        }
+        return searchedPost.get();
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post addPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Post updatePost(@PathVariable long id, @RequestBody Post updPost) {
+        var searchedPost = postRepository.findById(id);
+        if (searchedPost.isPresent()) {
+            var post = searchedPost.get();
+            post.setBody(updPost.getBody());
+            post.setTitle(updPost.getTitle());
+            return postRepository.save(post);
+        }
+        return updPost;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletePost(@PathVariable long id) {
+        var searchedPost = postRepository.findById(id);
+        if (searchedPost.isPresent()) {
+            var post = searchedPost.get();
+            commentRepository.deleteByPostId(post.getId());
+            postRepository.deleteById(id);
+        }
+    }
+}
+// END
