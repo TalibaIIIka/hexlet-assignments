09029616c51e5498c1062c2e3b0a9f404345b8ab
@hexlet/cli <support@hexlet.io>
2024-05-29 18:58:01 +0500

    submit java-spring-ru/search

--- java-spring-ru/search/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/search/src/main/java/exercise/controller/ProductsController.java
@@ -1,32 +1,20 @@
 package exercise.controller;
 
-import java.util.List;
-
 import exercise.dto.ProductCreateDTO;
 import exercise.dto.ProductDTO;
 import exercise.dto.ProductParamsDTO;
 import exercise.dto.ProductUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
 import exercise.mapper.ProductMapper;
-import exercise.model.Product;
+import exercise.repository.ProductRepository;
 import exercise.specification.ProductSpecification;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import exercise.exception.ResourceNotFoundException;
-import exercise.repository.ProductRepository;
-import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
